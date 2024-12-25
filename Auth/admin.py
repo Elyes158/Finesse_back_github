f6ab533dc3cf0ea56d86ca95cda7c7c44e3bceea
Elyes159 <elyesmlik307@gmail.com>
2024-12-25 18:51:44 +0100

    create app shop

--- Auth/admin.py
+++ Auth/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from Auth.models import UserProfile
+from Auth.models import AuthToken, UserProfile
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(AuthToken)
+
