d322b8c84939414b6200f2b1df98e91dcf9ad7cc
Elyes159 <elyesmlik307@gmail.com>
2025-01-08 22:02:10 +0100

    scd

--- Shop/models.py
+++ Shop/models.py
@@ -22,7 +22,7 @@ class SubCategory(models.Model):
     
 class Product(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="products")
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, related_name="products")
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
