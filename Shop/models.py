fe9007105c3724846f805aeef46650697efc0d83
Elyes159 <elyesmlik307@gmail.com>
2025-01-13 02:17:30 +0100

    final categories

--- Shop/models.py
+++ Shop/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
-    
+        
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to='product_images/')
